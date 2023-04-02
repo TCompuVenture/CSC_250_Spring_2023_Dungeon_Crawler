4d81ca21be6d05d741b7291b7b35181b52bb55f1
TCompuVenture <101987813+TCompuVenture@users.noreply.github.com>
2023-04-02 15:35:13 -0500

    Finished HW
    Made ball only continue in direction of travel once key pressed to go in that direction. Also caused new scene to be loaded when user hits exit.

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; //What we want to apply forces TO
+    private bool test = false;
     public GameObject northExit, southExit, eastExit, westExit; //Exposing exits
     public float movementSpeed = 40.0f; //Can tune in Unity editor b/c public
     // Start is called before the first frame update
@@ -19,14 +20,36 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow)) //If uparrow became pressed down during frame, DO something
+        if(Input.GetKeyDown(KeyCode.UpArrow) && test.Equals(false)) //If uparrow became pressed down during frame, DO something
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed); //direction = POSITION of north exit position vector3
+            movementSpeed = 0;
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow)) //ignore for right now - load next room, but with only appropriate exits enabled. We will have a scene that represents every room.
+        
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) 
         {
-            MasterData.count++;
-            SceneManager.LoadScene("DungeonRoom"); //Ask scene manager to load a scene named "DugeonRoom"
+            this.rb.AddForce(this.westExit.transform.position * movementSpeed); 
+            movementSpeed = 0;
         }
+        if(Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            this.rb.AddForce(this.eastExit.transform.position * movementSpeed); 
+            movementSpeed = 0;
+        }  
+       if(Input.GetKeyDown(KeyCode.DownArrow)) 
+        {
+            this.rb.AddForce(this.southExit.transform.position * movementSpeed); 
+            movementSpeed = 0;
+        }     
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+            if(other.gameObject.CompareTag("Exit"))  //other.tag.Equals("pickup")
+            {
+                print("found exit");
+                MasterData.count++;
+                SceneManager.LoadScene("DungeonRoom"); //Ask scene manager to load a scene named "DugeonRoom"
+
+            }
     }
 }
