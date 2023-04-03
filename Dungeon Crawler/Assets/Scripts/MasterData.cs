0ff5428ce841932c73022d7aeb6567340ae84b99
TCompuVenture <101987813+TCompuVenture@users.noreply.github.com>
2023-04-03 11:55:47 -0500

    Updated to reflect current state of code in class

--- Dungeon Crawler/Assets/Scripts/MasterData.cs
+++ Dungeon Crawler/Assets/Scripts/MasterData.cs
@@ -1,5 +1,6 @@
 public class MasterData //NOT Monobehavior - doesn't need start() or update() things! //Non-mono behaviors inherit from Object - not something Unity knows about
 {
     public static int count = 0; //Call with class NAME, so MasterData
+    public static string whereDidIComeFrom = "?";
 }
 //IS a singleton
--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; //What we want to apply forces TO
-    private bool test = false;
+    private bool isMoving;
     public GameObject northExit, southExit, eastExit, westExit; //Exposing exits
     public float movementSpeed = 40.0f; //Can tune in Unity editor b/c public
     // Start is called before the first frame update
@@ -14,40 +14,64 @@ public class PlayerController : MonoBehaviour
     {
         this.rb = this.GetComponent<Rigidbody>(); //Ask Player for HIS rigidbody object, save into rb
         print(MasterData.count); 
+        this.isMoving = false;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow) && test.Equals(false)) //If uparrow became pressed down during frame, DO something
+        if(Input.GetKeyDown(KeyCode.UpArrow) && !isMoving) //same as this.isMoving == false
         {
             this.rb.AddForce(this.northExit.transform.position * movementSpeed); //direction = POSITION of north exit position vector3
-            movementSpeed = 0;
+           // movementSpeed = 0;
+            this.isMoving = true;
         }
         
-        if(Input.GetKeyDown(KeyCode.LeftArrow)) 
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && !isMoving) 
         {
             this.rb.AddForce(this.westExit.transform.position * movementSpeed); 
-            movementSpeed = 0;
+           // movementSpeed = 0;
+            this.isMoving = true;
+
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow)) 
+        if(Input.GetKeyDown(KeyCode.RightArrow) && !isMoving) 
         {
             this.rb.AddForce(this.eastExit.transform.position * movementSpeed); 
-            movementSpeed = 0;
+           // movementSpeed = 0;
+            this.isMoving = true;
+
         }  
-       if(Input.GetKeyDown(KeyCode.DownArrow)) 
+       if(Input.GetKeyDown(KeyCode.DownArrow) && !isMoving) 
         {
             this.rb.AddForce(this.southExit.transform.position * movementSpeed); 
-            movementSpeed = 0;
+           // movementSpeed = 0;
+            this.isMoving = true;
         }     
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-            if(other.gameObject.CompareTag("Exit"))  //other.tag.Equals("pickup")
+            if(other.gameObject.CompareTag("Exit"))  //other.gameObject.tag.Equals("Exit")
             {
-                print("found exit");
+                    if(other.gameObject == this.northExit)
+                    {
+                        MasterData.whereDidIComeFrom = "north";
+                    }
+                    if(other.gameObject == this.southExit)
+                    {
+                        MasterData.whereDidIComeFrom = "south";
+                    }
+                    if(other.gameObject == this.eastExit)
+                    {
+                        MasterData.whereDidIComeFrom = "east";
+                    }
+                    if(other.gameObject == this.westExit)
+                    {
+                        MasterData.whereDidIComeFrom = "west";
+                    }
+                print(MasterData.whereDidIComeFrom);
                 MasterData.count++;
+                MasterData.whereDidIComeFrom = "north";
                 SceneManager.LoadScene("DungeonRoom"); //Ask scene manager to load a scene named "DugeonRoom"
 
             }
